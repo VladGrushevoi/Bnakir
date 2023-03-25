047f12db430f054bae08b9e6d2522ba83a2cc107
vladg <vlad.grush.99@gmail.com>
2023-03-25 17:49:10 +0200

    Create init migration

---
+++ Kisa.Persistence/Migrations/20230325154348_InitMigration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kisa.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "KisaCards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CardNumber = table.Column<string>(type: "text", nullable: false),
+                    CVV = table.Column<string>(type: "text", nullable: false),
+                    CountryName = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateOnly>(type: "date", nullable: false),
+                    ExpireTo = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KisaCards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KisaMain",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Balance = table.Column<double>(type: "double precision", nullable: false),
+                    CommissionInCountry = table.Column<float>(type: "real", nullable: false),
+                    CommissionBetweenCountry = table.Column<float>(type: "real", nullable: false),
+                    CommissionBetweenCardSystem = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KisaMain", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KisaCards");
+
+            migrationBuilder.DropTable(
+                name: "KisaMain");
+        }
+    }
+}
