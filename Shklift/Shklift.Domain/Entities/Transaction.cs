385c3b2a502915fc3fd72ffca45d344a798f5dd4
vladg <vlad.grush.99@gmail.com>
2023-04-02 10:59:47 +0300

    add validation to card number

--- Shklift/Shklift.Domain/Entities/Transaction.cs
+++ Shklift/Shklift.Domain/Entities/Transaction.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
 using Shklift.Domain.Common;
 
 namespace Shklift.Domain.Entities;
@@ -8,12 +7,12 @@ public sealed class Transaction : BaseEntity
 {
     [Required(ErrorMessage = "The field from card number is required")]
     [StringLength(16, ErrorMessage = "Length of card number must be 16 numbers")]
-    [CreditCard]
+    [RegularExpression("\\d{16}", ErrorMessage = "Card number must be contain 16 chars")]
     public string FromCardNumber { get; set; }
     
     [Required(ErrorMessage = "The field to card number is required")]
     [StringLength(16, ErrorMessage = "Length of card number must be 16 numbers")]
-    [CreditCard]
+    [RegularExpression("\\d{16}", ErrorMessage = "Card number must be contain 16 chars")]
     public string ToCardNumber { get; set; }
     
     [Required(ErrorMessage = "Amount money is required")]
