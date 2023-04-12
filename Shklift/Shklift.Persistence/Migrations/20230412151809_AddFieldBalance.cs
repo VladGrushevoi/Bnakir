39d4717697288be6f892e05f47bc7aefbaae1cf3
vladg <vlad.grush.99@gmail.com>
2023-04-12 19:11:59 +0300

    Create column balance

---
+++ Shklift/Shklift.Persistence/Migrations/20230412151809_AddFieldBalance.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shklift.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldBalance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Balance",
+                table: "ShkliftSettings",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "ShkliftSettings");
+        }
+    }
+}
