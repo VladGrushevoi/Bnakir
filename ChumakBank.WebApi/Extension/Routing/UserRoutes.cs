0c0c32adbc756f35129e6e377183c3de0d623f0b
vladg <vlad.grush.99@gmail.com>
2023-04-20 16:49:38 +0300

    add update user endpoint

--- ChumakBank.WebApi/Extension/Routing/UserRoutes.cs
+++ ChumakBank.WebApi/Extension/Routing/UserRoutes.cs
@@ -1,4 +1,5 @@
-﻿using ChumakBank.Application.Features.UserFeatures;
+﻿using ChumakBank.Application.Features.UserFeatures.CreateUserFeature;
+using ChumakBank.Application.Features.UserFeatures.UpdateUserFeature;
 using MediatR;
 
 namespace ChumakBank.WebApi.Extension.Routing;
@@ -8,6 +9,8 @@ public static class UserRoutes
     public static RouteGroupBuilder UserRouteBuild(this RouteGroupBuilder group)
     {
         group.MapPost("/create", async (CreateUserRequest req, IMediator mediator, CancellationToken cls)
+            => await mediator.Send(req, cls));
+        group.MapPatch("/update", async (UpdateUserRequest req, IMediator mediator, CancellationToken cls) 
             => await mediator.Send(req, cls));
         return group;
     }
