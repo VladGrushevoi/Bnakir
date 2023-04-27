c2cadde902bc7166ad00ac092333af9e176218dc
vladg <vlad.grush.99@gmail.com>
2023-04-27 12:53:42 +0300

    Create new repositories

--- ChumakBank.Application/Repositories/BaseUnitOfWork.cs
+++ ChumakBank.Application/Repositories/BaseUnitOfWork.cs
@@ -4,11 +4,15 @@ public abstract class BaseUnitOfWork
 {
     public readonly IUserRepository UserRepository;
     public readonly IChumakRepository ChumakRepository;
+    public readonly IKisaCardRepository KisaCardRepository;
+    public readonly IMapsterCardRepository MapsterCardRepository;
 
-    protected BaseUnitOfWork(IUserRepository userRepository, IChumakRepository chumakRepository)
+    protected BaseUnitOfWork(IUserRepository userRepository, IChumakRepository chumakRepository, IMapsterCardRepository mapsterCardRepository, IKisaCardRepository kisaCardRepository)
     {
         UserRepository = userRepository;
         ChumakRepository = chumakRepository;
+        MapsterCardRepository = mapsterCardRepository;
+        KisaCardRepository = kisaCardRepository;
     }
 
     public abstract Task SaveAsync(CancellationToken cls);
