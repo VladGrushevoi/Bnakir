7e5b4830236a4af10cbc9b1c9e409a64ddf4ae32
vladg <vlad.grush.99@gmail.com>
2023-05-05 21:08:41 +0300

    Create bat file for running apis

---
+++ run-all-webApi.bat
@@ -0,0 +1,5 @@
+﻿
+start /d "./Kisa.WebApi" dotnet run -c Instance1 args
+start /d "./MapsterCard" dotnet run -c Instance2 args
+start /d "./Shklift/Shklift.WebApi" dotnet run -c Instance3 args
+start /d "./ChumakBank.WebApi" dotnet run -c Instance4 args
