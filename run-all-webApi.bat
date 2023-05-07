36c0cf39b8c6d4d1a4ab3627efd54ae0e12be169
vladg <vlad.grush.99@gmail.com>
2023-05-07 12:51:20 +0300

    change bat file

--- run-all-webApi.bat
+++ run-all-webApi.bat
@@ -1,5 +1,5 @@
 ﻿
-start /d "./Kisa.WebApi" dotnet run -c Instance1 args
-start /d "./MapsterCard" dotnet run -c Instance2 args
-start /d "./Shklift/Shklift.WebApi" dotnet run -c Instance3 args
-start /d "./ChumakBank.WebApi" dotnet run -c Instance4 args
+start  cmd.exe /c dotnet run -c Instance1 --project "MapsterCard"
+start cmd.exe /c dotnet run -c Instance2 --project "Kisa.WebApi"
+start cmd.exe /c dotnet run -c Instance3 --project "Shklift/Shklift.WebApi"
+start cmd.exe /c dotnet run -c Instance4 --project "ChumakBank.WebApi"
