1cf4e126a71ce43b0efa380f805a42ed6b1fded3
vladg <vlad.grush.99@gmail.com>
2023-05-08 11:45:03 +0300

    Create Initial migration

---
+++ KozakBank.Persistence/Migrations/20230508084109_InitMigration.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KozakBank.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "KozakInfos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Balance = table.Column<float>(type: "real", nullable: false),
+                    CommissionValue = table.Column<float>(type: "real", nullable: false),
+                    BankIdentifier = table.Column<string>(type: "character varying(4)", maxLength: 4, nullable: false),
+                    CreatedOnly = table.Column<DateOnly>(type: "date", nullable: false),
+                    UpdatedAt = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KozakInfos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionHistories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CardId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AmountMoney = table.Column<float>(type: "real", nullable: false),
+                    Commission = table.Column<float>(type: "real", nullable: false),
+                    CreatedOnly = table.Column<DateOnly>(type: "date", nullable: false),
+                    UpdatedAt = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Surname = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    CreatedOnly = table.Column<DateOnly>(type: "date", nullable: false),
+                    UpdatedAt = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KisaCards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedOnly = table.Column<DateOnly>(type: "date", nullable: false),
+                    UpdatedAt = table.Column<DateOnly>(type: "date", nullable: false),
+                    IdFromCardSystem = table.Column<Guid>(type: "uuid", nullable: false),
+                    Balance = table.Column<float>(type: "real", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KisaCards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_KisaCards_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MapsterCards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedOnly = table.Column<DateOnly>(type: "date", nullable: false),
+                    UpdatedAt = table.Column<DateOnly>(type: "date", nullable: false),
+                    IdFromCardSystem = table.Column<Guid>(type: "uuid", nullable: false),
+                    Balance = table.Column<float>(type: "real", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MapsterCards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MapsterCards_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KisaCards_UserId",
+                table: "KisaCards",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MapsterCards_UserId",
+                table: "MapsterCards",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KisaCards");
+
+            migrationBuilder.DropTable(
+                name: "KozakInfos");
+
+            migrationBuilder.DropTable(
+                name: "MapsterCards");
+
+            migrationBuilder.DropTable(
+                name: "TransactionHistories");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
