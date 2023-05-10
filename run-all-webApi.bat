405fda2992c391a9a5e73297aef49afefb87a65b
vladg <vlad.grush.99@gmail.com>
2023-05-10 17:53:46 +0300

    Update script file

--- run-all-webApi.bat
+++ run-all-webApi.bat
@@ -1,5 +1,6 @@
 ﻿
-start  cmd.exe /c dotnet run -c Instance1 --project "MapsterCard"
+start cmd.exe /c dotnet run -c Instance1 --project "MapsterCard"
 start cmd.exe /c dotnet run -c Instance2 --project "Kisa.WebApi"
 start cmd.exe /c dotnet run -c Instance3 --project "Shklift/Shklift.WebApi"
 start cmd.exe /c dotnet run -c Instance4 --project "ChumakBank.WebApi"
+start cmd.exe /c dotnet run -c Instance5 --project "KozakBank.WebApi"
