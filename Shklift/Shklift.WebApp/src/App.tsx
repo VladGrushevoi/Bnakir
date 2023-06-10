82dc5e5b0123a6f8b01f9104aefac37c33782fc6
vladg <vlad.grush.99@gmail.com>
2023-06-10 15:31:39 +0300

    init webapp for shklift system

---
+++ Shklift/Shklift.WebApp/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+function App(): React.JSX.Element {
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline ">
+                Hello React-Ts
+            </h1>
+            <h1 className="text-3xl">
+                HEllo 2
+            </h1>
+        </>
+    )
+}
+
+export default App
