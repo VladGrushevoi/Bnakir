4d17bdbfbd5002e45cb5c4764b303ba46f811ff1
vladg <vlad.grush.99@gmail.com>
2023-06-11 12:48:38 +0300

    create header

--- Shklift/Shklift.WebApp/src/App.tsx
+++ Shklift/Shklift.WebApp/src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import './App.css'
+import { Header } from './components/Header/Header'
 
 function App(): React.JSX.Element {
 
     return (
         <>
-            <h1 className="text-3xl font-bold underline ">
-                Hello React-Ts
-            </h1>
-            <h1 className="text-3xl">
-                HEllo 2
-            </h1>
+            <Header />
         </>
     )
 }
---
+++ Shklift/Shklift.WebApp/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+interface HeaderProps {
+
+}
+
+export const Header = ({} : HeaderProps) => {
+
+    return (
+        <>
+            <div className="container flex justify-center w-full h-28 rounded-b-xl shadow-slate-400 shadow-md bg-lime-200 ">
+                <h1 className="w-full h-full pt-7 font-medium tracking-widest text-5xl font-mono text-center">SHKLIFT</h1>
+            </div>
+        </>
+    )
+}
