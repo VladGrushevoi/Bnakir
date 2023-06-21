501d8f5973da547756e37c39df855c1fe85249fa
vladg <vlad.grush.99@gmail.com>
2023-06-21 14:40:39 +0300

    receipt page in procces

--- Shklift/Shklift.WebApp/src/pages/ReceiptPage/ReceiptPage.tsx
+++ Shklift/Shklift.WebApp/src/pages/ReceiptPage/ReceiptPage.tsx
@@ -6,7 +6,21 @@ export const ReceiptPage = ({} : ReceiptPageProps) => {
 
     return (
         <>
-            <h1>RECEIPT</h1>
+            <div className="w-[80%] h-[60%] m-auto mt-6 border rounded-2xl text-center justify-center  shadow-md ">
+                <div className="block text-4xl mt-0 rounded-t-2xl py-2 bg-green-300">
+                    <span className="w-full h-full rounded-full mx-4 px-3 font-extrabold text-red-500 bg-red-800">X</span>
+                    <span className="tracking-widest font-semibold">КВИТАНЦІЯ</span>
+                </div>
+                <div className="w-full h-full flex mt-6 px-6 text-center">
+                    <div className="w-1/2 h-[80%] border rounded-lg">
+                        <span className="block">Відправник</span>
+                    </div>
+                    <span className="mx-2"></span>
+                    <div className="w-1/2 h-[80%] border rounded-l">
+                        <span className="block">Отримувач</span>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
